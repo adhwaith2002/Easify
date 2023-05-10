5c68e5237b9830abe1de54552f513f5662e2f230
adhwaith <adhwaithadhwaith@gmail.com>
2023-05-10 14:16:10 +0530

    third commit

--- app/src/main/java/com/example/easify/userdashboard.java
+++ app/src/main/java/com/example/easify/userdashboard.java
@@ -1,18 +1,52 @@
 package com.example.easify;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class userdashboard extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
 
+public class userdashboard extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+   DrawerLayout drawerLayout;
+   NavigationView navigationView;
+   Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_userdashboard);
+        drawerLayout = findViewById(R.id.drawerlayout);
+        navigationView = findViewById(R.id.navview);
+        toolbar = findViewById(R.id.toolbar);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
+
         setSupportActionBar(toolbar);
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else{
+            super.onBackPressed();
+        }
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return true;
     }
 }
